0a4967a6a2cc8d1062720367a9d741603a4baf72
teethawat-3900 <teethawat.k@kkumail.com>
2025-02-13 18:38:46 +0700

    fix bugs

--- exercise/gui3/SimpleFormV7.java
+++ exercise/gui3/SimpleFormV7.java
@@ -1,14 +1,23 @@
 package kumying.teethawat.exercises.gui3;
 
+import java.awt.Toolkit;
 import java.awt.event.*;
 
 import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
 
 public class SimpleFormV7 extends SimpleFormV6 implements ItemListener {
     public SimpleFormV7(String title) {
         super(title);
     }
 
+    @Override
+    public void addMenus() {
+        super.addMenus();
+        newMI.setMnemonic(KeyEvent.VK_N);
+        openMI.setMnemonic(KeyEvent.VK_O);
+        quitMI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+    }
     @Override
     public String getMessage() {
         StringBuffer msg = new StringBuffer();
@@ -31,6 +40,7 @@ public class SimpleFormV7 extends SimpleFormV6 implements ItemListener {
         if (python.isSelected()) { msg.append(python.getText() + " "); }
         if (java.isSelected()) { msg.append(java.getText() + " "); }
         if (javascript.isSelected()) { msg.append(javascript.getText() + " "); }
+        if (msg.isEmpty()) { return null; }
         return msg.substring(0, msg.length()-1).toString();
     }
     @Override
@@ -39,6 +49,10 @@ public class SimpleFormV7 extends SimpleFormV6 implements ItemListener {
         male.addItemListener(this);
         female.addItemListener(this);
         other.addItemListener(this);
+        //
+        newMI.addItemListener(this);
+        openMI.addItemListener(this);
+        quitMI.addItemListener(this);
     }
     @Override
     public void itemStateChanged(ItemEvent e) {
@@ -51,6 +65,12 @@ public class SimpleFormV7 extends SimpleFormV6 implements ItemListener {
             JOptionPane.showMessageDialog(this, female.getText()+msg, "Message", JOptionPane.INFORMATION_MESSAGE);
         } else if (sorce == other && state == ItemEvent.SELECTED) {
             JOptionPane.showMessageDialog(this, other.getText()+msg, "Message", JOptionPane.INFORMATION_MESSAGE);
+        }
+        //
+        if (sorce == newMI) {
+            JOptionPane.showMessageDialog(this, male.getText()+msg, "Menu New is clicked", JOptionPane.INFORMATION_MESSAGE);
+        } else if (sorce == quitMI) {
+            System.exit(0);
         }
     }
     
